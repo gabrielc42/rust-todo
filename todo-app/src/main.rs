56a36e3542cd4ea460eca56fbd5405379670e0bb
gabrielc42 <clark.gabe97@gmail.com>
2022-11-08 18:59:56 -0600

    create method

--- todo-app/src/main.rs
+++ todo-app/src/main.rs
@@ -1,3 +1,26 @@
+use std::{fs::{OpenOptions}, io::{Write}};
+use rocket::serde::{Deserialize, json::Json};
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Task<'r> {
+    item: &'r str
+}
+
+#[post("/addtask", data="<task>")]
+fn add_task(task: Json<Task<'_>>) -> &'static str {
+    let mut tasks = OpenOptions::new()
+                    .read(true)
+                    .append(true)
+                    .create(true)
+                    .open("tasks.txt")
+                    .expect("unable to access tasks.txt");
+    let task_item_string = format!("{}\n", task.item);
+    let task_item_bytes = task_item_string.as_bytes();
+    task.write(task_item_bytes).expect("unable to write to tasks.txt");
+    "Task added successfully"
+}
+
 #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -7,5 +30,5 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, add_task])
 }
