67d2b47313dafeccbebd0bc4df8be020e981b4da
gabrielc42 <clark.gabe97@gmail.com>
2022-11-09 13:44:32 -0600

    read http success

--- todo-app/src/main.rs
+++ todo-app/src/main.rs
@@ -1,27 +1,46 @@
-use std::{fs::{OpenOptions}, io::{Write}};
-use rocket::serde::{Deserialize, json::Json};
+use rocket::serde::{json::Json, Deserialize, Serialize};
+use std::{fs::OpenOptions, io::Write};
+use std::io::BufReader;
+use std::io::BufRead;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
 struct Task<'r> {
-    item: &'r str
+    item: &'r str,
 }
 
-#[post("/addtask", data="<task>")]
+#[post("/addtask", data = "<task>")]
 fn add_task(task: Json<Task<'_>>) -> &'static str {
     let mut tasks = OpenOptions::new()
+        .read(true)
+        .append(true)
+        .create(true)
+        .open("tasks.txt")
+        .expect("unable to access tasks.txt");
+    let task_item_string = format!("{}\n", task.item);
+    let task_item_bytes = task_item_string.as_bytes();
+    tasks
+        .write(task_item_bytes)
+        .expect("unable to write to tasks.txt");
+    "Task added successfully"
+}
+
+#[get("/readtasks")]
+fn read_tasks() -> Json<Vec<String>> {
+    let tasks = OpenOptions::new()
                     .read(true)
                     .append(true)
                     .create(true)
                     .open("tasks.txt")
-                    .expect("unable to access tasks.txt");
-    let task_item_string = format!("{}\n", task.item);
-    let task_item_bytes = task_item_string.as_bytes();
-    tasks.write(task_item_bytes).expect("unable to write to tasks.txt");
-    "Task added successfully"
+                    .expect("unable to access tasks.txt");  
+    let reader = BufReader::new(tasks);
+    Json(reader.lines()
+            .map(|line| line.expect("could not read line"))
+            .collect())
 }
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -30,5 +49,5 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, add_task])
+    rocket::build().mount("/", routes![index, add_task, read_tasks])
 }
